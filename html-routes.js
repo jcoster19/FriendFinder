6116a2ccc0d5d86a091eb4bd9a376ab8d994a2cc
jcoster19 <jcoster19@gmail.com>
2017-05-08 13:32:05 -0400

    Add files via upload

---
+++ html-routes.js
@@ -0,0 +1,17 @@
+var path = require("path");
+
+
+module.exports = function(app) {
+
+//HTML GET Request
+
+	app.get("/survey", function(req, res) {
+		res.sendFile(path.join(_dirname + "/../public/survey.html"));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}
